edb360bcbeb02448e02700fa999d6bd4b048635a
DaBinQwQ <72700807+DaBinQwQ@users.noreply.github.com>
2024-05-08 14:57:11 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "GPT - DaBinQwQ",
+  description: "Build your own AI assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
